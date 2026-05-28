6794dd3fd5627e17c94abb78bbd3bf9f98a0aca9
Maria Eduarda <0001204539@senaimgaluno.com.br>
2026-05-28 10:11:25 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let TrianguloLado1 = 10;
+let TrianguloLado2 = 20;
+let TrianguloLado3 = 30;
+
+let soma = TrainguloLado1 + TrianguloLado2 > TrianguloLado3
+
+if (soma) {
+    alert("É possível formar um triângulo com os lados informados.");
+
+}
