7a3adf2f8bcb621fcd1d2fb410fad43b7f305601
Maria Eduarda <0001204539@senaimgaluno.com.br>
2026-05-28 10:22:12 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+let idade =  Number(prompt("Digite a sua idade: "))
+
+if (idade >= 18 && idade <= 59) {
+    alert("Você é um adulto.")
+}
+if (idade >= 60) {
+    alert("Você é um idoso.")
+}
+if (idade >=13 && idade <= 17) {
+    alert("Você é um adolescente.")
+}
+if (idade >= 0 && idade <= 12) {
+    alert("Você é uma criança.")
+}
