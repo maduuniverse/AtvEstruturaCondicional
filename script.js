6c1bbe6ca161145f4c701bb9919464634cffcf24
Maria Eduarda <0001204539@senaimgaluno.com.br>
2026-05-28 10:27:33 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+let base = Number(prompt("Digite a base do retângulo:"));
+let altura = Number(prompt("Digite a altura do retângulo:"));
+let area = base * altura;
+alert("A área do retângulo é: " + area);
