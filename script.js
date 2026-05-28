9e832b4e74564f91d24b33aa2d62a986d5e5ca6f
Maria Eduarda <0001204539@senaimgaluno.com.br>
2026-05-28 09:54:32 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let numero1 = prompt("Digite um número:")
+let numero2 = prompt("Digite outro número:")
+if (numero1 > numero2) {
+    alert("O número " + numero1 + " é maior que o número " + numero2)
+}
+else if (numero2 > numero1) {
+    alert("O número " + numero2 + " é maior que o número " + numero1)
+}
