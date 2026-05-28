cbb16865ea29ba7a19e1f01c87cc52b6fb2d2803
Maria Eduarda <0001204539@senaimgaluno.com.br>
2026-05-28 10:32:58 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+let valor = Number(prompt("Digite o valor de um produto: "))
+let valorDesconto = valor * 0.1
+let valorFinal = valor - valorDesconto
+
+alert("O valor do desconto é R$" + valorDesconto + " e o valor final do produto é R$" + valorFinal)
