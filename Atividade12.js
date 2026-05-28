a54f9a9a94a94da1d10e1aba1b5519318a37b3c4
Maria Eduarda <0001204539@senaimgaluno.com.br>
2026-05-28 10:17:13 -0300

    Update Atividade12.js

--- Atividade12.js
+++ Atividade12.js
@@ -1,8 +1,8 @@
 let nota = Number(prompt("Digite a nota do aluno: "));
-if (nota >= 6) {
+if (nota >= 6 && nota <= 10) {
     alert("Aluno aprovado!");
 }
-if (nota = 5) {
+if (nota >= 5 && nota < 6) {
     alert("Aluno em recuperação!");
 }
 if (nota < 5) {
