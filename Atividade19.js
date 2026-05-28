9004c3287457f910d1cc94c8d77b52d7b0c4cec6
Maria Eduarda <0001204539@senaimgaluno.com.br>
2026-05-28 10:35:00 -0300

    Create Atividade19.js

---
+++ Atividade19.js
@@ -0,0 +1,8 @@
+let n1 = Number(prompt("Digite o primeiro número:"));
+let n2 = Number(prompt("Digite o segundo número:"));
+
+if (n1 > 0 && n2 > 0) {
+    alert("Soma: " + (n1 + n2));
+} else {
+    alert("A soma não será feita");
+}
