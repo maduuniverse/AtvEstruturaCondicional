88e308d59d2e5534b9db175200903b8a56c1f17b
Maria Eduarda <0001204539@senaimgaluno.com.br>
2026-05-28 10:19:08 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+let n1 = Number(prompt("Digite o primeiro número:"));
+let n2 = Number(prompt("Digite o segundo número:"));
+
+if (n1 % n2 === 0) {
+    alert("O primeiro número é múltiplo do segundo");
+} else {
+    alert("O primeiro número não é múltiplo do segundo");
+}
