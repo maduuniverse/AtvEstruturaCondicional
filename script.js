6662185b2395fe6445605d98d74b396696621b4f
Maria Eduarda <0001204539@senaimgaluno.com.br>
2026-05-28 10:15:46 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let nota = Number(prompt("Digite a nota do aluno: "));
+if (nota >= 6) {
+    alert("Aluno aprovado!");
+}
+if (nota = 5) {
+    alert("Aluno em recuperação!");
+}
+if (nota < 5) {
+    alert("Aluno reprovado!");
+}
