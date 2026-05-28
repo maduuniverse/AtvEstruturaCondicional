cad0a6869ab55a2065e9491ae526802ad0a73267
Maria Eduarda <0001204539@senaimgaluno.com.br>
2026-05-28 09:52:44 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let numero = prompt("Digite um número:")
+if (numero + 10 >=0) {
+    alert("A soma desse número com 10 é positiva.")
+}
+if (numero + 10 < 0) {
+    alert("A soma desse número com 10 é negativa.")
+}
