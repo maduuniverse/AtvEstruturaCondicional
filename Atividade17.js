639c2ecc3f19c75b5c6790c1c9c6f44cbcabfe77
Maria Eduarda <0001204539@senaimgaluno.com.br>
2026-05-28 10:34:18 -0300

    Create Atividade17.js

---
+++ Atividade17.js
@@ -0,0 +1,10 @@
+let letra = prompt("Digite uma letra:");
+
+if (
+    letra === "a" || letra === "e" || letra === "i" ||
+    letra === "o" || letra === "u"
+) {
+    alert("É uma vogal");
+} else {
+    alert("Não é uma vogal");
+}
